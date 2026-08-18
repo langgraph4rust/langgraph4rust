f12f5fe714eab9b113a704847ed57130d9393082
TJ.MA <tj.ma@tj.ma>
2026-08-18 08:46:26 +0800

    add multi startnode

--- src/core/state_graph.rs
+++ src/core/state_graph.rs
@@ -359,9 +359,8 @@ impl<S: AgentState + Send + Sync> StateGraph<S> {
             let nodes = self.get_node_by_keys(&current)?;
             if !nodes.is_empty() {
                 self.batch_apply(nodes, Arc::clone(&state)).await?;
-            } else {
-                current = self.get_next_node_key(&current, state.as_ref())?;
             }
+            current = self.get_next_node_key(&current, state.as_ref())?;
         }
         Ok(())
     }
